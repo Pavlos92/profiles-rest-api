64c9b0dd96969dcdbfa05ee956909c4d3c93978a
Pavlos Vlachospyros <apostoloupavlou31@gmail.com>
2024-03-14 14:10:17 +0200

    Addes login API

--- profiles/profiles_api/views.py
+++ profiles/profiles_api/views.py
@@ -6,6 +6,8 @@ from .serializers import HelloSerializer, UserProfileSerializer
 from .models import UserProfile
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from .permissions import UpdateOwnProfile
 
 
@@ -94,3 +96,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
